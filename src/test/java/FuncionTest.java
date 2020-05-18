34a78553afd8dc6a4460fd994e4183c521987035
LORENZO <l.devia02@ufromail.cl>
2020-05-18 17:53:15 -0400

    Base de jUnit

---
+++ src/test/java/FuncionTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FuncionTest {
+    private Funcion a;
+    @BeforeEach
+    void setUp() {
+        a = new Funcion();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void esPalindromo() {
+    }
+}
